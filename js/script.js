504bdb141593ef3faa9d6b1bc6ad0d0013b53074
Victor Miguel <victordudasom@hotmail.com>
2023-07-12 16:39:32 -0300

    Primeiro commit, scroll suave ja refatorado

---
+++ js/script.js
@@ -0,0 +1,25 @@
+import ScrollSuave from './modules/scroll-suave.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTolltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"');
+scrollSuave.init();
+
+initAccordion();
+initTabNav();
+initModal();
+initTolltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
+initAnimacaoScroll();
