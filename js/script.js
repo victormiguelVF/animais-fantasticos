3b957e67ad5eba7f567e2267baf99c9c1370c679
Victor Miguel <victordudasom@hotmail.com>
2023-07-13 16:26:23 -0300

    animacao ao scroll refeita

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
+import ScrollAnima from './modules/scroll-anima.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"');
 scrollSuave.init();
@@ -24,10 +24,13 @@ modal.init();
 
 const tooltip = new Tolltip('[data-tooltip]');
 tooltip.init();
+
+const ScrollAnima = new ScrollAnima('[data-anime="scroll"]');
+ScrollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initAnimacaoScroll();
 
 fetchAnimais('../../animaisapi.json', 'numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco'); 
